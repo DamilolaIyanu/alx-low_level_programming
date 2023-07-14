9a55dacbb3932db61c0206c7cd739bd9cc568544
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-07-14 16:36:39 -0700

    printing numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0)
+ *
+ */
+int main(void)
+{
+	int baseTen = 0;
+
+	while (baseTen <= 10)
+	{
+		putchar(baseTen);
+		baseTen++;
+	}
+	putchar('\n');
+	return (0);
+}
