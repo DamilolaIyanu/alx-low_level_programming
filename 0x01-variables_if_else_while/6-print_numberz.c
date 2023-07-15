2238c8ad86bcf04d300f7fe051819de05fefaefd
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-07-14 17:56:32 -0700

    printing numbers using alphabets

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0)
+ *
+ */
+int main(void)
+{
+	int alphabets = '0';
+
+	while (alphabets <= '9')
+	{
+		putchar(alphabets);
+		alphabets++;
+	}
+
+		putchar('\n');
+		return (0);
+}
