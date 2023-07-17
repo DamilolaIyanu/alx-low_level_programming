ebbf31a644baa193089a9372d7ee43076b1479ce
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-07-17 07:45:37 -0700

    two two  number pairing

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always (0)
+ *
+ */
+int main(void)
+{
+	int num1 = '0';
+	int num2 = '0';
+	int num3 = '0';
+	int num4 = '0';
+
+	while (num1 <= '9')
+	{
+		while (num2 <= '9')
+		{	num3 = '0';
+			while (num3 <= '9')
+			{	num4 = '0';
+				while (num4 <= '9')
+				{
+					if (num1 < num2 && num2 < num3)
+					{
+						putchar(num1);
+						putchar(num2);
+						putchar(' ');
+						putchar(num3);
+						putchar(num4);
+
+					if ((num1 + num2) < (num3 + num4))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					}
+				num4++;
+				}
+			num3++;
+			}
+		num2++;
+		}
+	num1++;
+	num2 = '0';
+	}
+	putchar('\n');
+	return (0);
+}
