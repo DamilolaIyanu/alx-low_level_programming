8bce01870a305c090dfde18684a0736038db611c
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-07-18 11:01:35 -0700

    absolute number function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - Prints the absolute integer
+ *
+ * @r: collects number
+ *
+ * Return: Returns the absolute integer
+ *
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (r * (-1));
+	}
+}
