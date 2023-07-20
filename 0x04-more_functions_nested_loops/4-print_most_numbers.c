aca4bc3697beb5b85038feca3991ee3e0f00595e
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-07-20 13:42:37 -0700

    prints digit without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_most_numbers - Print number except 2 and 4.
+ *
+ *Return: The value of num
+ *
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0');
+		}
+	}
+	_putchar('\n');
+}
+
