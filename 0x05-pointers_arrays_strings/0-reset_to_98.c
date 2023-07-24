d721be8c0eccee7ed246c92984f32093b0b9bc71
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-07-24 04:22:02 -0700

    function changes the value of an integer variable to another value

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include  "main.h"
+
+/**
+ *reset_to_98 - changes the value of an integer to 98
+ *
+ *@n: points to the numbers.
+ *
+ *
+ *return: Nothing
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+0x05-pointers_arrays_strings tasks
+projects on pointers, arrays and strings
