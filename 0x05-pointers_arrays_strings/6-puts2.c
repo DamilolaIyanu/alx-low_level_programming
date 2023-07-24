526ff9fbaf8e35eedbaf29285c0c3fa7a684422d
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-07-24 07:44:31 -0700

    priting thew other number

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *puts2 - Prints every other character of a string
+ *
+ *@str: Stores the string whose character is to be printed
+ *
+ *
+ *Return: Returns nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] >= 0; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
