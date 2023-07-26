5aae1efce787e1fea855063f3fb517991da3dec0
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-07-26 05:12:01 -0700

    appending strings without terminating null

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *_strcat - appends one string to another.
+ *@dest: it is the destination string
+ *@src: it is the source string
+ *Return: Returns nothing
+ */
+char *_strcat(char *dest, char *src)
+{
+	int dest_len = 0;
+	int i = 0;
+
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	while (src[i] != '\0')
+	{
+		dest[dest_len + i] = src[i];
+		i++;
+	}
+	dest[dest_len + i] = '\0';
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0x06-pointers_arrays_strings
+more task on pointers, arrays and strings.
+learning to find my own way.
