befcd67b3dc27ecc7d630b0ff437c45f39bf6763
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-07-26 19:03:34 -0700

    copy source string to dest string

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,20 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
+	int destlen = 0;
 
 	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i] = '\0';
+	if (i >= n)
+	{
+		dest[destlen - 1] = '\0';
+	}
+	else
+	{
+		dest[i] = '\0';
+	}
 
 	return (dest);
 }
