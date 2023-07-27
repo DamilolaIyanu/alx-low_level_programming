bbf6db53c708f7f68ac410975891be63c4faa76e
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-07-27 13:15:16 -0700

    encoding words

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *leet - function codes leeters as numbers
+ *@str: contains the items to code
+ *
+ *Return: Returns the coded words
+ */
+
+char *leet(char *str)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; a[j] != '\0'; j++)
+		{
+			if (str[i] == a[j])
+			{
+				str[i] = b[j];
+			}
+		}
+	}
+	return (str);
+}
