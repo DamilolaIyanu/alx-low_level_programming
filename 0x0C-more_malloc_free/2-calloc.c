73ab1785a0b5035d18c8e8eb216c3ade6cb566c2
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-08-11 04:04:10 -0700

    allocates memory for an array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_calloc - allocates memory for an array
+ *@nmemb: one of the grid
+ *@size: The size of the grid
+ *@Return: Return pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+	size_t i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(size * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < (nmemb * size); i++)
+		ptr[i] = 0;
+	return(ptr);
+}
