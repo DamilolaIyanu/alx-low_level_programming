975ca5b767f782b7feb7647368b392950500de0b
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-08-14 16:52:31 -0700

    prints struct dog type with some conditions

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,21 +11,20 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d)
 	{
-		;
-	}
-	if (!(*d).name)
-		printf("Name: (nil)");
-	else
-		printf("Name: %s\n", d->name);
+		if (!(*d).name)
+			printf("Name: (nil)");
+		else
+			printf("Name: %s\n", d->name);
 
-	if (!(*d).age)
-		printf("Age: (nil)");
-	else
-		printf("Age: %f\n", d->age);
-	if (!(*d).owner)
-		printf("Owner: (nil)");
-	else
-		printf("Owner: %s\n", d->owner);
+		if (!(*d).age)
+			printf("Age: (nil)");
+		else
+			printf("Age: %f\n", d->age);
+		if (!(*d).owner)
+			printf("Owner: (nil)");
+		else
+			printf("Owner: %s\n", d->owner);
+	}
 }
