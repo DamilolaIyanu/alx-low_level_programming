ccf6ab4508e34c8be5568ee807e8face3023abd7
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-08-14 14:51:12 -0700

    a new type struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ *struct dog - a new type template
+ *@name: the name member
+ *@age: the age member
+ *@owner: the owner member
+ *
+ * Description: a new type.
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
