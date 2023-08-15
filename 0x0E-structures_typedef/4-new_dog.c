150f9c37c4891725e47c9f1cb83c2d7bf6a12dcf
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-08-14 18:03:04 -0700

    print struct dog update

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ *new-dog - function creates new dog
+ *@name: the dog name
+ *@age: the dog age
+ *@owner: the dog owner
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
