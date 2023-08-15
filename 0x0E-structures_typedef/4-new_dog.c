2c97a75cb9456b60f6239a469e063b2e1fc1d97d
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-08-14 18:55:35 -0700

    creating a new dog and initailizing it

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,11 +1,54 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "dog.h"
 
 /**
- *new-dog - function creates new dog
+ *new_dog - function creates new dog
  *@name: the dog name
  *@age: the dog age
  *@owner: the dog owner
+ *Return: Returns pointer to new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
+{
+	unsigned int name1, owner1, i;
+	dog_t *dog;
+
+	if (owner == NULL || name == NULL)
+		return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (name1 = 0; name[name1] != '\0'; name1++)
+		;
+	name1++;
+	dog->name = malloc(sizeof(char) * name1);
+	if (dog->name == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < name1; i++)
+		dog->name[i] = name[i];
+	dog->age = age;
+	for (owner1 = 0; owner[owner1] != '\0'; owner1++)
+		;
+	owner1++;
+	dog->owner = malloc(sizeof(char) * owner1);
+	if (dog->owner == NULL)
+	{
+		free(dog->owner);
+		free(dog);
+		return (NULL);
+	}
+	for (i = 0; i < owner1; i++)
+		dog->owner[i] = owner[i];
+	return (dog);
+
+
+}
