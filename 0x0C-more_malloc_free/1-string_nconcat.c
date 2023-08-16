52e31bcc6ba133cada47a4fbd0ffd74ae5bdc7f1
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-08-16 02:30:41 -0700

    string concatenation

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- *string_nconcat - conatenates the n of string 2 to string 1
+ *string_nconcat - concatenates the n of string 2 to string 1
  *@s1: the first string
  *@s2: the second string
  *@n: the number of character to be added to the first string
@@ -27,18 +27,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	else
 	{
 		j = 0;
-		while (s2[i] != '\0')
+		while (s2[j] != '\0')
 			j++;
 	}
-	j = n;
+	if (j > n)
+		j = n;
 	ptr = malloc(sizeof(char) * (i + j + 1));
 	if (ptr == NULL)
 		return (NULL);
 
 	for (k = 0; k < i; k++)
-		ptr[k] = s1[i];
+		ptr[k] = s1[k];
 	for (k = 0; k < j; k++)
-		ptr[k + i] = s2[j];
+		ptr[k + i] = s2[k];
 	ptr[i + j] = '\0';
 	return (ptr);
 }
---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,24 @@
+#include < stdlib.h>
+#include "main.h"
+#include <stdio.h>
+
+/**
+ 
+ *
+ *
+ *
+ *
+ *
+ */
+int checkd(char *c)
+{
+	int a = 0; 
+
+	while (c[a])
+	{
+		if (c[a] < '0' || c[a] > '9'
+				return (0);
+		i++;
+	}
+	return (1);
+}
