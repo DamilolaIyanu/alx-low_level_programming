a3857cd0b53a17bb8fb1ae3d6ffd4db513d1c3e1
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-08-16 01:45:07 -0700

    prints the name of its source code

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -3,10 +3,10 @@
 /**
  *main - prints the name of the file it was compiled from
  *
- *Return: Return (0) on success
+ *Return: Return (0)
  */
 
-int main()
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
