143e66faed0067b996646ef93d1b8f0ddf599305
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-08-25 10:44:31 -0700

    adding a new node to the beginning of a linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -6,11 +6,12 @@
 /**
  *len - finds the length of the string
  *@str: the string whose length is to be calculated
- *Return: Returns the length of the string 
+ *Return: Returns the length of the string
  */
 int len(const char *str)
 {
 	int n = 0;
+
 	while (str[n] != '\0')
 	{
 		n++;
@@ -46,13 +47,13 @@ list_t *make_node(const char *str)
  *add_node - adds the newly created node to the beginning of the list
  *@head: the pointer to the bginning of the list
  *@str: the string in the node
- *Return: Returns the address to the new node 
+ *Return: Returns the address to the new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *nunode;
 
-	nunode= make_node(str);
+	nunode = make_node(str);
 	if (nunode == NULL)
 		return (NULL);
 	nunode->next = *head;
