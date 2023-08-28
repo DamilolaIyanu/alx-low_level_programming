533d6d00428c074007ff857253a1d3b188218846
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-08-28 07:54:24 -0700

    writing assembly code

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,5 +1,6 @@
 section .data
     hello db "Hello, Holberton", 0
+    newline db 10  ; ASCII code for newline
 
 section .text
     global main
@@ -9,5 +10,10 @@ main:
     push rbp
     mov rdi, hello
     call printf
+
+    ; Print a newline
+    mov rdi, newline
+    call printf
+
     pop rbp
     ret
