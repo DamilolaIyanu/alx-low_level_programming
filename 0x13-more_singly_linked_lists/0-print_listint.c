fc5df31383ec3735ebe1a7360f239633e8c989ae
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-09-01 14:30:28 -0700

    printing all the list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,12 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	const listint_t *i = h;
-
 	size_t count = 0;
 
-	while (i != NULL)
+	while (h != NULL)
 	{
-		printf("%d\n", i->n);
-		i = i->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		count++;
 	}
 	return (count);
