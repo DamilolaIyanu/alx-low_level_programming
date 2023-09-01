56f25a8b00b3da182e6421541f2e4aea25382d6e
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-09-01 06:15:07 -0700

    print the binary representation of an unsigned long int

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_binary: prints the binary representation of an unsigned long int
+ *
+ *@n: the number whose binary rep is to be printed
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int setBit = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int start = 0;
+
+	while (setBit > 0)
+	{
+		if ((n &setBit) || start)
+		{
+			_putchar((n & setBit) ? '1' : '0');
+			start = 1;
+		}
+		setBit >>= 1;
+	}
+
+	if (!start)
+		_putchar('0');
+}
