f7769e38f9e32eba839c21f777e9f0967d449d2b
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-09-05 04:03:30 -0700

    copies content from file to file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,6 +10,26 @@
 #define BUFFER_SIZE 1024
 
 /**
+ *error_handler - prints the error message
+ *@message: the error message
+ *@exit_code: the exit
+ */
+void error_handler(const char *message, int exit_code)
+{
+	dprintf(2, "%s\n", message);
+	exit(exit_code);
+}
+/**
+ *set_file_permissions - this function set the file permnission
+ *@filename: the pointer to the file
+ *Return: returns the permission
+ */
+int set_file_permissions(const char *filename)
+{
+	return (chmod(filename, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH));
+}
+
+/**
  *main - copies a file from one place to anothr
  *@argc: the argument count
  *@argv: the argument vector
@@ -45,35 +65,27 @@ int copy_file(const char *file_from, const char *file_to)
 
 	fd_from = open(file_from, O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", file_from);
-		close(fd_from);
-		exit(98); }
-	fd_to = open(file_to, O_WRONLY
-			| O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+		error_handler("Error: Can't read from file", 98);
+	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR
+			| S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (fd_to == -1)
 	{
-		dprintf(2, "Error: Can't write to file %s\n", file_to);
-		close(fd_from);
-		exit(99); }
+		error_handler("Error: Can't write to file", 99);
+	}
 	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		bytes_written = write(fd_to, buffer, bytes_read);
 		if (bytes_written == -1)
 		{
-			dprintf(2, "Error: Can't write to file %s\n", file_to);
-			close(fd_from);
-			close(fd_to);
-			exit(99); } }
+			error_handler("Error: Can't write to file", 99);
+		}
+	}
 	if (bytes_read == -1)
 	{
-		 dprintf(2, "Error: Can't read from file %s\n", file_from);
-		 close(fd_from);
-		 close(fd_to);
-		 exit(98); }
+		error_handler("Error: Can't read from file", 98);
+	}
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
-		dprintf(2, "Error: Can't close file descriptor\n");
-		exit(100); }
-	return (0);
+		error_handler("Error: Can't close file descriptor", 100);
+	}
 }
