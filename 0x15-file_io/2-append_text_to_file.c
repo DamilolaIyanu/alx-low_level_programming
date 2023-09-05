0f353fd1d6a793790f46b0608d699f3cdb19ef27
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-09-04 17:50:24 -0700

    appents a text content to an already existing file

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "main.h"
+#include <fcntl.h>
+#include <string.h>
+/**
+ *append_text_to_file - adds to the end of the file if it existed
+ *@filename: the pointer to the file
+ *@text_content: the pointer to the text we are adding to the file
+ *Return: returns 1 or -1 depending on if it was successful or not
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		if (bytes_written == -1)
+		{
+			close(fd);
+			return (-1);
+		}
+	}
+	close(fd);
+	return (1);
+
+}
