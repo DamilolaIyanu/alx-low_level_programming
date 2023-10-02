85e85047c8fc5cb14b80ea947111fac0bd1ffabf
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-10-02 05:36:53 -0700

    reads from a file

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -18,12 +18,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 		return (0);
-
 	fd = open(filename, O_RDONLY);
 
 	if (fd == -1)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 
 	if (buffer == NULL)
@@ -31,7 +29,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	bytes_read = read(fd, buffer, letters);
 
 	if (bytes_read == -1)
@@ -40,7 +37,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
 	if (bytes_written == -1 || bytes_written != bytes_read)
@@ -49,10 +45,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	free(buffer);
 	close(fd);
 
 	return (bytes_written);
 }
-
