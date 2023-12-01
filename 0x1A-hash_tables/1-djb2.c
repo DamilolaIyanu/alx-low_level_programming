3ce21229ff23e3b44f7da16809043cce75a11f06
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-12-01 13:41:50 -0800

    the djb2 implementation

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,8 +1,18 @@
 #include "hash_tables.h"
 /**
- *
- *
- *
- *
+ *hash_djb2 - the djb2 algorithm
+ *@str: the pointer to the string
+ *Return: the value of hash
  */
-unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	int c;
+	unsigned long int hash;
+
+	hash = 5381;
+	while((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return(hash);
+}
