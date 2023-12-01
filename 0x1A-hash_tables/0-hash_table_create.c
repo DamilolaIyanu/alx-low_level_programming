5cd45420fcfa86d21878fa448aff20ef9c4941ef
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-12-01 13:26:27 -0800

    creating a hash table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,5 +1,11 @@
 #include "hash_tables.h"
 
+/**
+ *hash_table_create - creates the hash table
+ *@size: the size of the hash table
+ *Return: returns the hash table created
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	unsigned long int i;
