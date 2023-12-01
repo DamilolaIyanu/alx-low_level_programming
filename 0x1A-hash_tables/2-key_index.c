d524e6f0ac2b8df495a92ad88f8d196fb675c35a
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-12-01 13:55:49 -0800

    using djb2 to find key index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,12 @@
+#include "hash_tables.h"
+
+/**
+ *key_index - returns the indext of the given key
+ *@key: the specified key whose index is returned
+ *@size: the size of the array of the hash table
+ *Return: returns the index of the key
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return(hash_djb2(key) % size);
+}
