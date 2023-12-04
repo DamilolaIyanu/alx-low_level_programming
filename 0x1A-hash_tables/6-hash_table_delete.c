4eb6014c51970a0563460dfda2063d810ccc2363
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-12-04 03:09:52 -0800

    deletes hash table

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+/**
+ *hash_table_delete - deletes hash table
+ *@ht: the hash table to be deleeted
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	hash_node_t *next;
+	unsigned long int i;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		while (ht->array[i] != NULL)
+		{
+			next = ht->array[i]->next;
+			free(ht->array[i]->key);
+			free(ht->array[i]->value);
+			free(ht->array[i]);
+			ht->array[i] = next;
+		}
+	}
+	free(ht->array);
+	ht->array = NULL;
+	ht->size = 0;
+	free(ht);
+}
