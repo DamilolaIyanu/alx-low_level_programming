732b23908f3cb5592c60942d371d3c9a8503fcec
DamilolaIyanu <damilolaiyanu45@gmail.com>
2023-12-04 02:45:59 -0800

    prints a hash tablewq

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ *hash_table_print - prints a hash table
+ *@ht: pointer to the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *temp;
+	unsigned long int i;
+	char flag = 0;
+
+	if (ht == NULL || ht->array == NULL)
+		return;
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			if (flag == 1)
+				printf(", ");
+			printf("'%s' : '%s'", temp->key, temp->value);
+			flag = 1;
+			temp = temp->next;
+		}
+	}
+	printf("}\n");
+}
